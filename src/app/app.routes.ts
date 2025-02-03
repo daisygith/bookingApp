7202f414b1548d64ca41e6c9c3ce1f955277a293
daisygith <daisygith@users.noreply.github.com>
2025-02-03 10:10:03 +0100

    created first component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'main-page',
+    loadChildren: () =>
+      import('./main-page/main-page.routes').then((m) => m.mainPageRoutes)
+  }
+];
