619172cdce1b815579a90a3981ff79ae7d2a5b88
daisygith <daisygith@users.noreply.github.com>
2025-02-03 11:00:04 +0100

    created first component

--- src/app/main-page/components/main-page/main-page.component.ts
+++ src/app/main-page/components/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
-import {Component} from "@angular/core";
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [],
   templateUrl: './main-page.component.html',
-  styleUrl: 'main-page.component.scss'
+  styleUrl: './main-page.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class MainPageComponent{}
