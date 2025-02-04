e53b01010513c92513d84f994a38b499ca112208
daisygith <daisygith@users.noreply.github.com>
2025-02-04 17:15:31 +0100

    added some styles to main-page

--- src/app/main-page/components/main-page/main-page.component.ts
+++ src/app/main-page/components/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule,MatIconModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
   encapsulation: ViewEncapsulation.None
